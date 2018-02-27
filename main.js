f2d53b48169525b74ecf41326c26a43e9899cd40
Julia <yulya_9060@mail.ru>
2018-02-27 17:17:54 +0300

    осталось доделать скрытие лишек

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ var checkbox = document.querySelectorAll(' .checkbox_hidden');
 Array.prototype.filter.call(checkbox,function(check)
 {
     check.addEventListener('click', function(event){
-        console.log(this.parentNode.parentNode.parentNode);
-            this.parentNode.parentNode.parentNode.parentNode.classList.add('courses__item--checked');
+        console.log(this.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.querySelectorAll('.courses__item'));
+            this.parentNode.parentNode.parentNode.parentNode.parentNode.querySelector('.courses_hover').classList.toggle("courses_hover--checked");
+            this.parentNode.parentNode.parentNode.parentNode.parentNode.querySelector('.courses__desc').classList.toggle("courses__desc--checked");
+            this.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.querySelectorAll('.courses__item').classList.add("courses__item--checked");
     }) 
 })
