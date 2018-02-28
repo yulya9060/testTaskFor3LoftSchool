ca297ed7ea52cab968aa168ed18a370db83b5c05
Julia <yulya_9060@mail.ru>
2018-02-28 17:07:44 +0300

    хрень творится

--- main.js
+++ main.js
@@ -6,17 +6,19 @@ Array.prototype.filter.call(checkbox,function(check)
 {
     check.addEventListener('click', function(event){
         console.log('checkbox ',   this.parentNode.parentNode.parentNode.parentNode.parentNode);
-           this.parentNode.parentNode.parentNode.parentNode.parentNode.querySelector('courses_hover').classList.toogle('.courses_hover--checked');
-          //this.parentNode.parentNode.parentNode.parentNode.parentNode.querySelector('.courses__desc').classList.toogle("courses__desc--checked");
-         //  this.parentNode.parentNode.parentNode.parentNode.parentNode.classList.add("courses__item--checked");
+        console.log('checkbox2 ',   this.outerHTML);
+        console.log('checkbox3',     this.parentNode.parentNode.parentNode.parentNode.parentNode.querySelector('.courses__desc').classList);
+           this.parentNode.parentNode.parentNode.parentNode.parentNode.querySelector('.courses_hover').classList.toggle('courses_hover--checked');
+           this.parentNode.parentNode.parentNode.parentNode.parentNode.querySelector('.courses__desc').classList.toggle("courses__desc--checked");
+           this.parentNode.parentNode.parentNode.parentNode.parentNode.classList.add("courses__item--checked");
             liChecked = document.querySelectorAll('.courses__list > .courses__item--checked');
             if (this.parentNode.parentNode.parentNode.parentNode.parentNode.classList.contains('courses__item--checked')){
                 count++;
                 console.log('1count ',count);
                 console.log('2liChecked ',liChecked);
-                setTimeout(clearCheck(liChecked,count,elements,check),3000);
+                setTimeout(clearCheck(liChecked,count,elements,check),4000);
             }
-            
+            console.log('checkbox3',     this.parentNode.parentNode.parentNode.parentNode.parentNode.querySelector('.courses__desc').classList);
         }) 
 });
 
